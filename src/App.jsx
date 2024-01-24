b137e69e0e9e293cea87c182d6bfe6477661d8fb
nafi-ullah <shafinnafiullah@gmail.com>
2024-01-24 16:33:55 +0600

    skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
 const App = () => (
-  <main></main>
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1
+    wide:padding-r padding-b">
+      Hero
+    </section>
+
+    <section className="padding">
+      Popular Producs
+    </section>
+
+    <section className="padding">
+      SuperQuality
+    </section>
+
+    <section className="padding-x
+    py-10">
+      Services
+    </section>
+
+    <section className="padding">
+      Special
+    </section>
+
+
+  </main>
 );
 
 export default App;
