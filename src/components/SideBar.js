3a90501ef8730a146701e4c10f371931c20ccffb
nafi-ullah <shafinnafiullah@gmail.com>
2024-02-03 16:32:02 +0600

    one button

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,17 +2,21 @@ import React from 'react'
 
 const SideBar = () => {
   return (
-    <div className="w-1/6 h-screen left-0 top-0 absolute bg-slate-900" >
+    <div className="w-1/6 h-screen  bg-slate-900 flex flex-col" >
 
-<div className="w-[1440px] h-[1024px] relative bg-white">
-    <div className="w-[271px] h-[1024px] left-0 top-0 absolute bg-slate-900" />
-    <div className="w-[223px] h-[45px] left-[24px] top-[100px] absolute">
-      <img className="w-[34px] h-9 left-[9px] top-0 absolute" src="https://via.placeholder.com/34x36" />
-      <div className="w-[223px] h-[45px] left-0 top-0 absolute rounded-[7px] border border-red-50" />
+
+    <div className="w-[310px] h-[45px] my-16 mx-4 flex rounded-[7px] border border-red-50">
+      <img className="w-[34px] h-9 m-1" src="https://via.placeholder.com/34x36" />
+      <div className=" text-white text-xl px-10 p-2 ">My Account</div>
     </div>
-    <div className="w-[271px] h-1 left-0 top-[965px] absolute bg-zinc-300" />
-    <div className="left-[86px] top-[113px] absolute text-white text-base font-bold font-['Inter']">My Account</div>
-  </div>
+
+    <div className="w-full h-10 bg-stone-900 rounded-[3px] flexm py-2 px-16" >
+  
+    <div className=" text-white text-xl  ">Prescription Help</div>
+    
+   </div>
+   
+  
 
 
     </div>
